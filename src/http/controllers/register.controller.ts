62e6c17694adf4a3a672bc61a8856737ce032129
Pedro Souza <pedro.olegario23@gmail.com>
2025-11-04 14:21:39 -0300

    ✨feat: user creator transformed into a service

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,9 +1,11 @@
+import { registerControllerUseCase } from '@/services/register.controller'
 import { FastifyRequest, FastifyReply } from 'fastify'
-import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
-import { hash } from 'bcryptjs'
 
-export async function registerController(request: FastifyRequest, reply: FastifyReply) {
+export async function registerController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
   const registerBodySchema = z.object({
     name: z.string(),
     email: z.email(),
@@ -12,25 +14,11 @@ export async function registerController(request: FastifyRequest, reply: Fastify
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
-  const password_hash = await hash(password, 6)
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
-
-  if(userWithSameEmail) {
+  try {
+    await registerControllerUseCase({ name, email, password })
+  } catch (error) {
     return reply.status(409).send()
   }
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
-  })
 
   return reply.status(201).send()
 }
