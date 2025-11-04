7b5dede58ce167edeace67050d77ec72274c3098
Pedro Souza <pedro.olegario23@gmail.com>
2025-11-04 15:17:48 -0300

    ✨feat: dependency inversion principle

--- src/http/controllers/register-controller.ts
+++ src/http/controllers/register-controller.ts
@@ -1,4 +1,5 @@
-import { registerControllerUseCase } from '@/services/register.controller'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+import { RegisterService } from '@/services/register-service'
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
 
@@ -15,7 +16,10 @@ export async function registerController(
   const { name, email, password } = registerBodySchema.parse(request.body)
 
   try {
-    await registerControllerUseCase({ name, email, password })
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const registerService = new RegisterService(prismaUsersRepository)
+
+    await registerService.execute({ name, email, password })
   } catch (error) {
     return reply.status(409).send()
   }
