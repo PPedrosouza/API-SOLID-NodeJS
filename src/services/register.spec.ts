952d08eb32544bac35688cbc2320f7afa422b0d3
Pedro Souza <pedro.olegario23@gmail.com>
2025-11-04 20:43:12 -0300

    🧪test: first switch for user unit testing

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,24 +1,25 @@
 import { expect, describe, it } from 'vitest'
 import { RegisterService } from './register-service'
 import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 
 describe('Register Service', () => {
-  it('should hash user password upon registration', async () => {
-    const registerService = new RegisterService({
-      async findByEmail(email) {
-        return null
-      },
+  it('should be able to register', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerService = new RegisterService(usersRepository)
 
-      async create(data) {
-        return {
-          id: 'user-1',
-          name: data.name,
-          email: data.email,
-          password_hash: data.password_hash,
-          created_at: new Date(),
-        }
-      },
+    const { user } = await registerService.execute({
+      name: 'John Doe',
+      email: 'john.doe3@example.com',
+      password: '123456',
     })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should hash user password upon registration', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerService = new RegisterService(usersRepository)
 
     const { user } = await registerService.execute({
       name: 'John Doe',
@@ -32,5 +33,26 @@ describe('Register Service', () => {
     )
 
     expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerService = new RegisterService(usersRepository)
+
+    const email = 'john.doe3@example.com'
+
+    await registerService.execute({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+
+    expect(() =>
+      registerService.execute({
+        name: 'John Doe',
+        email,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
