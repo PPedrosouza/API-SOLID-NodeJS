33998cabddfd13c587441a8af192304c06f02d0e
Pedro Souza <pedro.olegario23@gmail.com>
2025-11-04 13:30:19 -0300

    ✨feat: controller de registro

---
+++ src/http/controllers/register.controller.ts
@@ -0,0 +1,26 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { prisma } from '@/lib/prisma'
+import { z } from 'zod'
+
+export async function registerController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
+}
