03f7a59e659cc0bb615092482ae6e03c0ab057fe
Pedro Souza <pedro.olegario23@gmail.com>
2025-11-13 00:25:29 -0300

    ✨feat: jwt in fastify

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,12 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register-controller'
 import { authenticate } from './controllers/authenticate-contoller'
+import { profile } from './controllers/profile-controller'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
-
   app.post('/sessions', authenticate)
+
+  /** User Authenticated */
+  app.get('/me', profile)
 }
