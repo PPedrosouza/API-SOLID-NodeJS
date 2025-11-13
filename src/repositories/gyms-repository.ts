f603f5f49a60b6e444b7b819eea2b8735a2fce23
Pedro Souza <pedro.olegario23@gmail.com>
2025-11-12 22:20:03 -0300

    ✨feat: gyms in prisma repository

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -7,7 +7,7 @@ export interface FindManyNearByParams {
 
 export interface GymsRepository {
   findById(id: string): Promise<Gym | null>
-  create(data: Prisma.GymCreateInput): Promise<Gym>
-  searchMany(query: string, page: number): Promise<Gym[]>
   findManyNearby(params: FindManyNearByParams): Promise<Gym[]>
+  searchMany(query: string, page: number): Promise<Gym[]>
+  create(data: Prisma.GymCreateInput): Promise<Gym>
 }
